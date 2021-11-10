7db957ad5bf29f2c2f4105572dc1acb2c5ff3558
Yan Vaz <yan.vaz@localiza.com>
2021-11-09 22:46:01 -0300

    criando controller e routes

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,23 +1,22 @@
 const http = require('http');
 
-const users = require('./mocks/users')
+const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === request.url && routeObj.method === request.method
+  ))
 
-  if(request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    response.end(JSON.stringify(users))
+  if (route) {
+    route.handler(request, response)
   } else {
     response.writeHead(404, {
       'Content-Type': 'text/html'
     });
     response.end(`Cannot ${request.method} ${request.url}`)
   }
-  
-  
 })
 
 server.listen(3001, () => console.log('Server started at http://localhost:3001'));
